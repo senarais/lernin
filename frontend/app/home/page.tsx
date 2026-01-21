512332931ccd446a7d978b4788bd09475bd2f570
Abisena <abisena27@gmail.com>
2026-01-21 13:13:08 +0700

    added ipa section

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -18,16 +18,31 @@ const Home = () => {
     return (
         <div className="w-full h-dvh flex flex-col items-center bg-bg text-white overflow-x-hidden">
             <Navbar/>
+            {/* Hero Section */}
             <div className="relative w-[90%] h-fit flex justify-center items-center py-30">
-                <div className="max-w-[580px] z-10 flex flex-col gap-10">
-                    <h1 className="font-semibold text-4xl">Bimbingan Belajar UTBK untuk bantu kamu lolos <span className="text-third">PTN </span><span className="text-secondary">IMPIAN</span></h1>
+                <div className="max-w-[500px] z-10 flex flex-col gap-10">
+                    <h1 className="font-semibold text-4xl">Bimbingan Belajar UTBK untuk bantu kamu lolos <br /> <span className="text-third">PTN </span><span className="text-secondary">IMPIAN</span></h1>
                     <p className="text-2xl font-extralight">Tingkatkan Kemampuan di Lingkungan Profesional dan Raih Kesuksesan!</p>
-                    <a className="w-42 rounded-3xl flex items-center justify-center h-12 border border-secondary text-sm" href="/course">Coba Sekarang <img className="ml-2" src="/vector.svg" alt="" /></a>
+                    <a className="w-42 rounded-3xl flex items-center justify-center h-12 border border-secondary text-sm" href="/course">Coba Sekarang <img className="ml-2" src="/landing/vector.svg" alt="" /></a>
                 </div>
-                <img className="z-10" src="/hero.png" alt="hero image" />
+                <img className="z-10" src="/landing/hero.png" alt="hero image" />
                 <div className="absolute w-120 h-120 bg-third/40 blur-3xl -right-80 bottom-0 rounded-full z-1"></div>
                 <div className="absolute w-120 h-120 bg-third/40 blur-3xl -left-80  rounded-full z-1"></div>
             </div>
+
+            {/* IPA Section */}
+            <div className="relative w-[90%] h-fit flex justify-center items-center gap-20 py-30">
+                <img className="w-auto h-auto max-w-125" src="/landing/ipa.svg" alt="Ipa" />
+                <div className="max-w-[480px] z-10 flex flex-col gap-10">
+                    <h2 className="font-semibold text-4xl">Matapelajaran Dalam Jurusan IPA</h2>
+                    <p className="text-[18px] font-extralight">WooCommerce is developer friendly, too. Built with a REST API, WooCommerce is scalable and can integrate with virtually any service. Design a complex store from scratch, extend a store for a client, or simply add a single product to a WordPress site—your store, your way.</p>
+                    <a className="w-65 rounded-4xl flex items-center justify-center h-16 bg-primary text-white text-[18px]" href="/course">Materi Selengkapnya</a>
+                </div>
+                <div className="absolute w-120 h-120 bg-third/40 blur-3xl -right-60 rounded-full z-1"></div>
+            </div>
+
+
+
 
             <div className="w-full h-fit py-10 font-sans flex flex-col items-center">
                 <div className="">
