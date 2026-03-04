42cc1a13518a765ad1025c2090f019b5f17bef03
senarais <abisena27@gmail.com>
2026-03-05 00:30:43 +0700

    update session cookie to 14 days

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -73,7 +73,7 @@ export const googleAuthSuccess = async (req, res) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: 'lax',
-            maxAge: 1000 * 60 * 60 * 24
+            maxAge: 1000 * 60 * 60 * 24 * 14
         });
 
         res.json({ user });
